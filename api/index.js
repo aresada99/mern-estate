fbad98b1d21bf4752025ed1d136caa6ee926d7db
aresada99 <ares.ada.dora@gmail.com>
2024-02-07 16:12:10 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js'
+import authRouter from './routes/authRoute.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -13,9 +14,11 @@ mongoose.connect(process.env.MONGODB_URI)
 })
 
 const app = express();
+app.use(express.json())
 
 app.listen(3333, ()=> {
     console.log('Server is running on port 3333');
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
